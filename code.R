81994f9f04043145c1629375c4585ed5db830000
Šimon Trusina <simon.trusina@mvcr.cz>
2021-04-28 17:36:50 +0200

    pridani vzorce s odkazy mezi listy

--- code.R
+++ code.R
@@ -1,11 +1,10 @@
 library(tidyverse)
 library(jsonlite)
-library(lubridate)
 library(glue)
-library(writexl)
+library(openxlsx)
 
 # požadované agendy
-vyber <- c("A345")
+vyber <- c("A117")
 
 # stažení dat
 download.file("https://rpp-opendata.egon.gov.cz/odrpp/datovasada/sluzby.json", "data/sluzby.json")
@@ -15,45 +14,71 @@ download.file("https://rpp-opendata.egon.gov.cz/odrpp/datovasada/kategorie_ovm.j
 
 # zpracování dat
 source("R/etl.R", encoding = "UTF-8")
+# sluzby.bak <- sluzby
+# sluzby <- sluzby.bak
+
+# vytvoření listů v souboru
+wb <- createWorkbook()
+addWorksheet(wb, "info")
+addWorksheet(wb, "seznam úkonů")
+addWorksheet(wb, "návratnost")
 
 # TODO: info
-info <- tibble("Informace, jak používat tabulku" = "")
+info <- tibble(x1 = c("Informace, jak používat tabulku"))
 
 # TODO: přidání vzorce
-# nefunguje
+l <- nrow(sluzby)
+l <- l + 1
+sluzby <- sluzby %>% 
+  mutate(
+    'přínosy úředník' = "",
+    'přínosy klient' = ""
+  )
+# text vzorce
 vzorec <- tibble(x1 = c("návratnost (let)", "iniciální investiční výdaj (Kč)", "roční přínosy (Kč)", 
                    "roční provozní výdaje (Kč)", "", "roční přínosy pro VS (Kč)", 
                    "průměrná mzda ve VS (Kč)", "průměrná mzda ve VS na minutu (Kč)", 
                    "", "roční přínosy pro klienty (Kč)", "hrubý domácí produkt na obyvatele (Kč)", 
                    "HDP na obyvatele na minutu (Kč)"),
-                 x2 = 1)
-vzorec$x2[1] <- xl_formula('=B2/(B6+B10-B4)')
-vzorec$x2[2] <- 30000000
-vzorec$x2[3] <- xl_formula("=B6+B10")
-vzorec$x2[4] <- 6000000
-vzorec$x2[6] <- xl_formula("=SUMA('seznam úkonů'!T:T)")
-vzorec$x2[7] <- 40572
-vzorec$x2[8] <- xl_formula("=2*B7/173,92/60")
-vzorec$x2[10] <- xl_formula("=SUMA('seznam úkonů'!U:U)")
-vzorec$x2[11] <- 524867
-vzorec$x2[12] <- xl_formula("=B11/2008/60")
+                 x2 = c("", 30000000, "", 6000000, "", "", 40572, "", "", "", 524867, ""))
+vzorec$x2 <- as.integer(vzorec$x2)
 
-# funguje x problém s názvy sloupců
-sluzby <- sluzby.bak
-l <- nrow(sluzby)
-l <- l + 1
-sluzby <- sluzby %>% 
-  mutate(
-    'přínosy úředník' = xl_formula(glue('=O{2:l}*(P{2:l}*7.78+R{2:l})')),
-    'přínosy klient' = xl_formula(glue('=O{2:l}*(P{2:l}*4.36+S{2:l})'))
-    )
-write_xlsx(sluzby, "output/test.xlsx")
+# vyplnění dat do specifických listů
+writeData(wb, "info", x = info, colNames = FALSE)
+writeData(wb, "seznam úkonů", x = sluzby, colNames = TRUE, withFilter = T) # na první řádek přidán autom. filtr
+writeData(wb, "návratnost", x = vzorec, colNames = FALSE)
+
+# přínosy úředník
+writeFormula(wb, sheet = 2, x = glue("=O{2:l}*(P{2:l}*'návratnost'!B8+R{2:l})"), startCol = 20, startRow = 2)
+# přínosy klient
+writeFormula(wb, sheet = 2, x = glue("=O{2:l}*(P{2:l}*'návratnost'!B12+S{2:l})"), startCol = 21, startRow = 2)
+
+# návratnost
+writeFormula(wb, sheet = 3, x = '=B2/(B6+B10-B4)', startCol = 2, startRow = 1)
+writeFormula(wb, sheet = 3, x = '=B6+B10', startCol = 2, startRow = 3)
+writeFormula(wb, sheet = 3, x = "=SUM('seznam úkonů'!T:T)", startCol = 2, startRow = 6)
+writeFormula(wb, sheet = 3, x = "=2*B7/173.92/60", startCol = 2, startRow = 8)
+writeFormula(wb, sheet = 3, x = "=SUM('seznam úkonů'!U:U)", startCol = 2, startRow = 10)
+writeFormula(wb, sheet = 3, x = "=B11/2008/60", startCol = 2, startRow = 12)
+
+# nadefinování stylů buněk pro list seznam úkonů
+header <- createStyle(textDecoration = "bold")
+vyplnit <- createStyle(fgFill="yellow1")
+nevyplnovat <- createStyle(fgFill = "red")
+
+# přidání stylů, formátování
+addStyle(wb,2, header, rows=1, cols=1:21)
+addStyle(wb, 2, vyplnit, rows=2:l, col=15:19, gridExpand = T)
+addStyle(wb, 2, nevyplnovat, rows = 2:l, cols = 20:21, gridExpand = T)
+setColWidths(wb, 2, cols=1:21, widths = 22.73) # nadefinování šířky sloupců, může být nadefinováno i na spec. sloupce, záleží
+freezePane(wb, 2, firstRow = T) # ukotvení 1. řádku 
+
+# přidání stylů, formátování
 
 # TODO: přejmenování sloupců - ke službám přidat názvy
 
 # TODO: přidání seznamu problematických úkonů
 
 # uložení
-
-write_xlsx(list("info" = info, "seznam úkonů" = sluzby, "návratnost" = vzorec), paste0("output/ukony_", paste(vyber, collapse = "_"), ".xlsx"), col_names = TRUE)
+saveWorkbook(wb, file = paste0("output/ukony_", paste(vyber, collapse = "_"), ".xlsx"), overwrite = T)
 
