627408393e507a58d1dcb8b6ed50d98a40173915
Šimon Trusina <simon.trusina@mvcr.cz>
2021-05-06 13:11:20 +0200

    doplneni pro nasazeni

--- shiny_plan/deploy.R
+++ shiny_plan/deploy.R
@@ -1,5 +1,8 @@
 # deploy app
 tmp.enc <- options()$encoding
 options(encoding = "UTF-8")
+wd <- getwd()
+setwd(paste0(wd, "/shiny_plan"))
 rsconnect::deployApp(appFileManifest = "data/fileManifest.txt", appName = "plan_digitalizace", account = "egov", forceUpdate = TRUE)
 options(encoding = tmp.enc)
+setwd(wd)
